1874e37b79546028592ca88dd5e3613b9a3a0846
mayur4279 <mayurdhotre.a15@gmail.com>
2025-03-14 19:31:15 +0530

    code is upadated by mayur

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,10 +41,14 @@ function Home() {
           <li><Link to="/company">Company</Link></li>
 
           <li><Link to="/contact-us">Contact Us</Link></li>
-
         </ul>
-
       </nav>
+
+
+      
+
+
+
 
     </div>
   );
