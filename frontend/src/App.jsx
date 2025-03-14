f81e9efcfec8df64e9afdcf90f0b37bd5d02b4a4
mayur4279 <mayurdhotre.a15@gmail.com>
2025-03-14 15:34:33 +0530

    code is upadated by mayur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { motion } from 'framer-motion';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
