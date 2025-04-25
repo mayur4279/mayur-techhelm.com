0f327db3259230ce5c741d3d3750aabb8645439f
mayur4279 <mayurdhotre.a15@gmail.com>
2025-04-25 19:02:21 +0530

    code is upadated by mayur

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 5173,      // Your dev server port
+    strictPort: true, // Prevents fallback to another port
+    allowedHosts: [
+      'helmbytes.linkpc.net', // Your domain
+      'localhost',            // Local development
+    ],
+  },
 })
